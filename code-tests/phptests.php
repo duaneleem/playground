ba447771ac8c95f23da7a4c6b498d2b0933d915a
Duane Leem <duane@leemfamily.com>
2018-09-18 12:35:09 -0700

    PHP testing

---
+++ code-tests/phptests.php
@@ -0,0 +1,5 @@
+<?php
+  $moderators = array("webinato_help@webinato.freshdesk.com","ross@warriortrading.com","swingtradewarrior@gmail.com","ross.p.cameron@gmail.com","nyselydone@gmail.com","tfortisweb@gmail.com");
+
+  echo array_search("ross@warriortrading.com", $moderators);
+?>
