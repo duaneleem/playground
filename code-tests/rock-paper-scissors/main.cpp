d767495998ded2202620afae895fa3c6347d1f08
Duane Leem <duane@leemfamily.com>
2019-04-06 20:13:45 -0700

    Finished application.

--- code-tests/rock-paper-scissors/main.cpp
+++ code-tests/rock-paper-scissors/main.cpp
@@ -14,12 +14,13 @@ void printStatistics(int, int, int, int);
 void finalStatistics(int, int, int, int);
 void welcome();
 
+int p1Wins = 0, p2Wins = 0, ties = 0, rounds = 1, result;
+
 int main() {
   //declarations, housekeeping, welcome & directions
   welcome();
 
   char player1, player2, again, choice;
-  int p1Wins = 0, p2Wins = 0, ties = 0, rounds = 0, result;
 
   //start of do-while loop
   do {
@@ -28,6 +29,7 @@ int main() {
     cin >> choice;
     player1 = tolower(choice);
 
+    // Display error if some other letter was chosen.
     while (choice != 'r' && choice != 'p' && choice != 's' && choice != 'q') {
       cout << setw(49) << "> ERROR: only pick from {r, p, s, q} > ";
       cin >> choice;
@@ -37,17 +39,20 @@ int main() {
     switch(checkThrow(player1, generateP2toss())) {
       case 0:
         cout << setw(WI_2) << "> Tie!" << endl;
+        ties++;
         break;
       case 1:
         cout << setw(WI_2) << "> Lose!" << endl;
+        p2Wins++;
         break;
       case 2:
         cout << setw(WI_2) << "> Win!" << endl;
+        p1Wins++;
         break;
     } // switch(checkThrow(player1, generateP2toss()))
 
     printStatistics(p1Wins, p2Wins, ties, rounds);
-    finalStatistics(p1Wins, p2Wins, ties, rounds);
+    rounds++;
 
     //repeat program?
     cout << setw(46) << "> Play another entire game (Y/N)? > ";
@@ -62,6 +67,10 @@ int main() {
   } while (again == 'Y'); // end of do while
 
   cout << setw(31) << "> Game ending. . . .\n";
+
+  // Output statistics due to quitting.
+  finalStatistics(p1Wins, p2Wins, ties, rounds);
+  exit(0);
 
   return 0;
 }
@@ -97,6 +106,10 @@ int checkThrow(char player1, char player2) {
       else if (player2 == 's') result = 1;
       else result = 2;
       break;
+
+    case 'q':
+      finalStatistics(p1Wins, p2Wins, ties, rounds);
+      exit(0);
   }
 
   return result;
